313709ec065b93b158dbfd6ee20458f0956311bc
s1F102301628 <s1f102301628@iniad.org>
2024-04-23 06:07:02 +0900

    一日目終了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,36 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
     const newBoard = structuredClone(board);
     newBoard[y][x] = turnColor;
-    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
-      newBoard[y][x] = turnColor;
+    const directions = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+    ];
 
+    /* for (const direction of directions) {
+      for (let i = 1; i < 8; i++)
+        if (
+          board[y + direction[0]] !== undefined &&
+          board[y + direction[0]][x] === 2 / turnColor &&
+          board[y][x + direction[1]] === 2 / turnColor &&
+          board[y + direction[0] * i][x + direction[1] * i] === turnColor //最大7マス先まで見て同じ色があるかを確認する
+        ) {
+          console.log(y + direction[0] * i, x + direction[1] * i);
+          newBoard[y][x] = turnColor;
+          newBoard;
+        }
+    } */
+    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      
+      newBoard[y][x] = turnColor;
+      newBoard[y + 1][x] = turnColor;
       setTurnColor(2 / turnColor);
       setBoard(newBoard);
     }
