9717da1e41667090be9ab330d3042134f4a89769
s1F102301628 <s1f102301628@iniad.org>
2024-04-25 18:01:21 +0900

    オセロの基盤完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const clickHandler = (x: number, y: number) => {
@@ -27,59 +27,70 @@ const Home = () => {
       [0, -1],
     ];
 
-    /* for (const direction of directions) {
-      for (let i = 1; i < 8; i++)
-        if (
-          board[y + direction[0]] !== undefined &&
-          board[y + direction[0]][x] === 2 / turnColor &&
-          board[y][x + direction[1]] === 2 / turnColor &&
-          board[y + direction[0] * i][x + direction[1] * i] === turnColor //最大7マス先まで見て同じ色があるかを確認する
-        ) {
-          console.log(y + direction[0] * i, x + direction[1] * i);
+    for (let i = 0; i < 8; i++) {
+      if (
+        board[y + directions[i][0]] !== undefined &&
+        board[y + directions[i][0]][x + directions[i][1]] === 2 / turnColor
+      ) {
+        for (let j = 1; j < 8; j++) {
+          if (
+            board[y + directions[i][0] * j] !== undefined &&
+            board[y + directions[i][0] * j][x + directions[i][1] * j] === turnColor
+          ) {
+            for (let k = j; k > 0; k--) {
+              newBoard[y][x] = turnColor;
+              newBoard[y + directions[i][0] * k][x + directions[i][1] * k] = turnColor;
+              setTurnColor(2 / turnColor);
+              setBoard(newBoard);
+            }
+          }
           newBoard[y][x] = turnColor;
-          newBoard;
+          setTurnColor(2 / turnColor);
+          setBoard(newBoard);
         }
-    } */
-    for (let i = 1; y + i < 8; i++) {
-      if (
-        board[y + i] !== undefined && // 置こうとしたマスの下が存在するか確認
-        board[y + i][x] === 2 / turnColor && // 一つ下が相手の駒か確認
-        board[y + i + 1] !== undefined && // 下の行が存在するか確認
-        board[y + i + 1][x] === turnColor // 置こうとしたマスの下を探索し自分の駒があるか確認
-      ) {
-        for (let j = 1; j <= i; j++) {
-          newBoard[y + j][x] = turnColor; // 相手の駒が一つではない時すべてひっくり返す
-        }
-        newBoard[y][x] = turnColor;
-        newBoard[y + i][x] = turnColor;
-        setTurnColor(2 / turnColor);
-        setBoard(newBoard);
-        break;
-      } else if (board[y + i][x] !== 2 / turnColor) {
-        break; // 一つ下が相手の駒でない場合は置けないのでループを終了
-      }
-    }
-
-    for (let i = 1; y - i >= 0; i++) {
-      if (
-        board[y - i] !== undefined && // 置こうとしたマスの上が存在するか確認
-        board[y - i][x] === 2 / turnColor && // 一つ上が相手の駒か確認
-        board[y - i - 1] !== undefined && // 上の行が存在するか確認
-        board[y - i - 1][x] === turnColor // 置こうとしたマスの上を探索し自分の駒があるか確認
-      ) {
-        for (let j = 1; j <= i; j++) {
-          newBoard[y - j][x] = turnColor; // 相手の駒が一つではない時すべてひっくり返す
-        }
-        newBoard[y][x] = turnColor;
-        newBoard[y - i][x] = turnColor;
-        setTurnColor(2 / turnColor);
-        setBoard(newBoard);
-        break;
-      } else if (board[y - i][x] !== 2 / turnColor) {
-        break; // 一つ上が相手の駒でない場合は置けないのでループを終了
       }
     }
   };
+  //   for (let i = 1; y + i < 8; i++) {
+  //     if (
+  //       board[y + i] !== undefined && // 置こうとしたマスの下が存在するか確認
+  //       board[y + i][x] === 2 / turnColor && // 一つ下が相手の駒か確認
+  //       board[y + i + 1] !== undefined && // 下の行が存在するか確認
+  //       board[y + i + 1][x] === turnColor // 置こうとしたマスの下を探索し自分の駒があるか確認
+  //     ) {
+  //       for (let j = 1; j <= i; j++) {
+  //         newBoard[y + j][x] = turnColor; // 相手の駒が一つではない時すべてひっくり返す
+  //       }
+  //       newBoard[y][x] = turnColor;
+  //       newBoard[y + i][x] = turnColor;
+  //       setTurnColor(2 / turnColor);
+  //       setBoard(newBoard);
+  //       break;
+  //     } else if (board[y + i][x] !== 2 / turnColor) {
+  //       break; // 一つ下が相手の駒でない場合は置けないのでループを終了
+  //     }
+  //   }
+
+  //   for (let i = 1; y - i >= 0; i++) {
+  //     if (
+  //       board[y - i] !== undefined && // 置こうとしたマスの上が存在するか確認
+  //       board[y - i][x] === 2 / turnColor && // 一つ上が相手の駒か確認
+  //       board[y - i - 1] !== undefined && // 上の行が存在するか確認
+  //       board[y - i - 1][x] === turnColor // 置こうとしたマスの上を探索し自分の駒があるか確認
+  //     ) {
+  //       for (let j = 1; j <= i; j++) {
+  //         newBoard[y - j][x] = turnColor; // 相手の駒が一つではない時すべてひっくり返す
+  //       }
+  //       newBoard[y][x] = turnColor;
+  //       newBoard[y - i][x] = turnColor;
+  //       setTurnColor(2 / turnColor);
+  //       setBoard(newBoard);
+  //       break;
+  //     } else if (board[y - i][x] !== 2 / turnColor) {
+  //       break; // 一つ上が相手の駒でない場合は置けないのでループを終了
+  //     }
+  //   }
+  // };
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
